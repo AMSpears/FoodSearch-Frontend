6db63d8d8dede154c8b7d2892c9188270b3efb0f
Angie Spears <angie.spears809@gmail.com>
2018-01-12 10:50:33 -0500

    update backend variable

--- src/BackendVariable.js
+++ src/BackendVariable.js
@@ -1,5 +1,11 @@
-let backend =
-	// 'http://localhost:4000/'
-	'https://food-searches.herokuapp.com/'
+// let backend = 'http://localhost:4000/'
+// // 'https://food-searches.herokuapp.com/'
+
+let backend
+if (document.location.hostname === 'localhost') {
+	backend = 'http://localhost:4000/'
+} else {
+	backend = 'https://food-search-app.herokuapp.com/'
+}
 
 export default backend
